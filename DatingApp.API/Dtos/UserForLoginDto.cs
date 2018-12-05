dd2899da57d3661baf7babc192ad5c6a27795511
Varun Keshav Suresh <varun.suresh@aparinnosys.com>
2018-12-05 19:52:27 +0530

    Added universal error handling to angular as well

--- DatingApp.API/Dtos/UserForLoginDto.cs
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -1,8 +1,7 @@
 namespace DatingApp.API.Dtos
 {
     public class UserForLoginDto
-    {
-        
+    {        
         public string Username { get; set; }
         public string Password { get; set; }
     }
